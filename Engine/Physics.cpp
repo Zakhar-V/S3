723c65c31b010ef62902bac494d2557c37c92645
Zakhar-V <Zakhar-V@users.noreply.github.com>
2017-07-10 14:03:56 +0700

    Implement Physics system (incomplete yet!)

--- Engine/Physics.cpp
+++ Engine/Physics.cpp
@@ -1,6 +1,272 @@
 #include "Physics.hpp"
 
 //----------------------------------------------------------------------------//
+// Collider
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+Collider::Collider(void)
+{
+}
+//----------------------------------------------------------------------------//
+Collider::~Collider(void)
+{
+}
+//----------------------------------------------------------------------------//
+void Collider::SetCenter(const Vector2& _center)
+{
+	if (m_center != _center)
+	{
+		m_center = _center;
+		_UpdateShape();
+	}
+}
+//----------------------------------------------------------------------------//
+void Collider::_Bind(void)
+{
+	m_body = m_entity->GetComponent<Body>();
+}
+//----------------------------------------------------------------------------//
+void Collider::_Unbind(void)
+{
+	ASSERT(!m_fixture);
+
+	m_body = nullptr;
+}
+//----------------------------------------------------------------------------//
+void Collider::_Enable(void)
+{
+	if (m_fixture)
+		_UpdateFilter();
+}
+//----------------------------------------------------------------------------//
+void Collider::_Disable(void)
+{
+	if (m_fixture)
+		_UpdateFilter();
+}
+//----------------------------------------------------------------------------//
+void Collider::_Create(void)
+{
+	ASSERT(m_body && m_body->_Handle() && !m_fixture);
+
+	b2FixtureDef _def;
+	_def.shape = _Shape();
+	_def.userData = this;
+	_def.friction = m_friction;
+	_def.restitution = m_restitution;
+	_def.density = m_density;
+	_def.isSensor = m_trigger;
+
+	m_fixture = m_body->_Handle()->CreateFixture(&_def);
+	_UpdateFilter();
+}
+//----------------------------------------------------------------------------//
+void Collider::_Destroy(void)
+{
+	ASSERT(m_body && m_body->_Handle() && m_fixture);
+
+	m_body->_Handle()->DestroyFixture(m_fixture);
+	m_fixture = nullptr;
+}
+//----------------------------------------------------------------------------//
+void Collider::_UpdateFilter(void)
+{
+	ASSERT(m_fixture);
+
+	b2Filter _filter;
+	if (!IsEnabled())
+	{
+		_filter.groupIndex = 0;
+		_filter.maskBits = 0;
+		_filter.categoryBits = 0;
+	}
+
+	m_fixture->SetFilterData(_filter);
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// CircleCollider
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+void CircleCollider::SetRadius(float _radius)
+{
+	m_radius = _radius;
+	_UpdateShape();
+}
+//----------------------------------------------------------------------------//
+void CircleCollider::_UpdateShape(void)
+{
+	m_shape.m_p = { m_center.x, m_center.y };
+	m_shape.m_radius = m_radius * m_entity->GetTransform().Scale();
+}
+//----------------------------------------------------------------------------//
+void CircleCollider::_DebugDraw(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// BoxCollider
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+void BoxCollider::SetRect(const Rect& _rect)
+{
+	m_rect = _rect;
+	_UpdateShape();
+}
+//----------------------------------------------------------------------------//
+void BoxCollider::_UpdateShape(void)
+{
+	float _scale = m_entity->GetTransform().Scale() * .5f;
+	m_shape.SetAsBox(m_rect.Width() * _scale, m_rect.Height() * _scale, { m_center.x, m_center.y }, 0);
+}
+//----------------------------------------------------------------------------//
+void BoxCollider::_DebugDraw(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// EdgeCollider
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+void EdgeCollider::Set(const Vector2& _v0, const Vector2& _v1)
+{
+	m_points[0] = _v0;
+	m_points[1] = _v1;
+	ApplyPoints();
+}
+//----------------------------------------------------------------------------//
+void EdgeCollider::ApplyPoints(void)
+{
+	_UpdateShape();
+}
+//----------------------------------------------------------------------------//
+void EdgeCollider::_UpdateShape(void)
+{
+	float _scale = m_entity->GetTransform().Scale() * .5f;
+
+	Vector2 _v0 = m_points[0] * _scale + m_center;
+	Vector2 _v1 = m_points[1] * _scale + m_center;
+	m_shape.Set({ _v0.x, _v0.y }, { _v1.x, _v1.y });
+}
+//----------------------------------------------------------------------------//
+void EdgeCollider::_DebugDraw(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// PolygonCollider
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+void PolygonCollider::_UpdateShape(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+void PolygonCollider::_DebugDraw(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// ChainCollider
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+void ChainCollider::_UpdateShape(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+void ChainCollider::_DebugDraw(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+// Body
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
+void Body::_Bind(void)
+{
+	for (Collider* i = m_entity->GetComponent<Collider>(); i; i = i->GetNextComponent<Collider>())
+		i->m_body = this;
+}
+//----------------------------------------------------------------------------//
+void Body::_Unbind(void)
+{
+	ASSERT(!m_body);
+
+	for (Collider* i = m_entity->GetComponent<Collider>(); i; i = i->GetNextComponent<Collider>())
+		i->m_body = nullptr;
+}
+//----------------------------------------------------------------------------//
+void Body::_Enable(void)
+{
+	if (m_body)
+		m_body->SetActive(true);
+}
+//----------------------------------------------------------------------------//
+void Body::_Disable(void)
+{
+	if (m_body)
+		m_body->SetActive(false);
+}
+//----------------------------------------------------------------------------//
+void Body::_Create(void)
+{
+	ASSERT(m_world && !m_body);
+
+	b2BodyDef _def;
+	_def.userData = this;
+	_def.position = { 0, 0 };
+	_def.angle = 0;
+	_def.linearVelocity = { 0, 0 };
+	_def.angularVelocity = 0;
+	_def.linearDamping = 0;
+	_def.angularDamping = 0;
+	_def.allowSleep = true;
+	_def.awake = true;
+	_def.fixedRotation = false;
+	_def.bullet = false;
+	_def.type = b2_staticBody;
+	_def.active = IsEnabled();
+	_def.gravityScale = 1;
+
+	m_body = m_world->_Handle()->CreateBody(&_def);
+	//TODO set transform
+
+	for (Collider* i = m_entity->GetComponent<Collider>(); i; i = i->GetNextComponent<Collider>())
+		i->_Create();
+}
+//----------------------------------------------------------------------------//
+void Body::_Destroy(void)
+{
+	ASSERT(m_world && m_body);
+
+	for (Collider* i = m_entity->GetComponent<Collider>(); i; i = i->GetNextComponent<Collider>())
+		i->_Destroy();
+
+	m_world->_Handle()->DestroyBody(m_body);
+	m_body = nullptr;
+}
+//----------------------------------------------------------------------------//
+
+//----------------------------------------------------------------------------//
 // PhysicsWorld
 //----------------------------------------------------------------------------//
 
@@ -8,9 +274,73 @@
 void PhysicsWorld::Register(void)
 {
 	//Object::Register<PhysicsComponent>();
+	Object::Register<CircleCollider>();
+	Object::Register<BoxCollider>();
+	Object::Register<PolygonCollider>();
+	Object::Register<EdgeCollider>();
+	Object::Register<ChainCollider>();
+	Object::Register<Body>(Component::Single);
 
 	Object::Register<PhysicsWorld>();
 	AddDefaultSystem(TypeName, PHYSICS_PRIORITY);
+}
+//----------------------------------------------------------------------------//
+PhysicsWorld::PhysicsWorld(void) :
+	m_world(b2Vec2(0, -9.8f))
+{
+	
+}
+//----------------------------------------------------------------------------//
+PhysicsWorld::~PhysicsWorld(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+void PhysicsWorld::_Start(Scene* _scene)
+{
+
+}
+//----------------------------------------------------------------------------//
+void PhysicsWorld::_Stop(void)
+{
+
+}
+//----------------------------------------------------------------------------//
+void PhysicsWorld::_AddComponent(Component* _component)
+{
+	if (_component->IsTypeOf<Body>())
+	{
+		Body* _body = static_cast<Body*>(_component);
+		LL_LINK(m_firstBody, _body, m_prevBody, m_nextBody);
+
+		_body->m_world = this;
+		//if (_body->IsEnabled())
+			_body->_Create();
+	}
+	else if (_component->IsTypeOf<Collider>())
+	{
+		//Collider* _collider = static_cast<Collider*>(_component);
+		// do nothing
+	}
+}
+//----------------------------------------------------------------------------//
+void PhysicsWorld::_RemoveComponent(Component* _component)
+{
+	if (_component->IsTypeOf<Body>())
+	{
+		Body* _body = static_cast<Body*>(_component);
+		LL_UNLINK(m_firstBody, _body, m_prevBody, m_nextBody);
+
+		if (_body->IsEnabled())
+			_body->_Destroy();
+		_body->m_world = nullptr;
+	}
+	else if (_component->IsTypeOf<Collider>())
+	{
+		Collider* _collider = static_cast<Collider*>(_component);
+		if (_collider->m_body)
+			_collider->_Destroy();
+	}
 }
 //----------------------------------------------------------------------------//
 void PhysicsWorld::_Update(void)
@@ -19,6 +349,26 @@ void PhysicsWorld::_Update(void)
 //----------------------------------------------------------------------------//
 void PhysicsWorld::_PostUpdate(void)
 {
+}
+//----------------------------------------------------------------------------//
+void PhysicsWorld::BeginContact(b2Contact* _contact)
+{
+
+}
+//----------------------------------------------------------------------------//
+void PhysicsWorld::EndContact(b2Contact* _contact)
+{
+
+}
+//----------------------------------------------------------------------------//
+void PhysicsWorld::PreSolve(b2Contact* _contact, const b2Manifold* _oldManifold)
+{
+
+}
+//----------------------------------------------------------------------------//
+void PhysicsWorld::PostSolve(b2Contact* _contact, const b2ContactImpulse* _impulse)
+{
+
 }
 //----------------------------------------------------------------------------//
 
--- Engine/Physics.hpp
+++ Engine/Physics.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
 #include "Scene.hpp"
+#include "Box2D\Box2D.h"
+
+//----------------------------------------------------------------------------//
+// Defs
+//----------------------------------------------------------------------------//
+
+class Collider;
+class Body;
+class Joint;
 
 //----------------------------------------------------------------------------//
 // PhysicsComponent
@@ -14,46 +23,282 @@ public:
 	Family GetFamily(void) override final { return Family::Physics; }
 
 protected:
+
+	class PhysicsWorld * m_world = nullptr;
 };
 
 //----------------------------------------------------------------------------//
 // Collider
 //----------------------------------------------------------------------------//
 
-class Collider : public PhysicsComponent
+class Collider abstract : public PhysicsComponent
 {
 public:
+	RTTI("Collider");
+
+	//!
+	Collider(void);
+	//!
+	~Collider(void);
+
+	//!
+	void SetCenter(const Vector2& _center);
+	//!
+	const Vector2& GetCenter(void) { return m_center; }
+
+	//!
+	b2Fixture* _Handle(void) { return m_fixture; }
+	//!
+	virtual void _DebugDraw(void) { }
 
 protected:
+	friend class Body;
+	friend class PhysicsWorld;
+
+	//!
+	void _Bind(void) override;
+	//!
+	void _Unbind(void) override;
+	//!
+	void _Enable(void) override;
+	//!
+	void _Disable(void) override;
+
+	//!
+	void _Create(void);
+	//!
+	void _Destroy(void);
+	//!
+	void _UpdateFilter(void);
+
+	//!
+	virtual b2Shape* _Shape(void) = 0;
+	//!
+	virtual void _UpdateShape(void) = 0;
+
+	Body* m_body = nullptr;
+	b2Fixture* m_fixture = nullptr;
+
+	Vector2 m_center = { 0, 0 };
+	float m_friction = .2f;
+	float m_restitution = 0;
+	float m_density = 0;
+	bool m_trigger = false;
 };
 
 //----------------------------------------------------------------------------//
-// RigidBody
+// CircleCollider
 //----------------------------------------------------------------------------//
 
-class RigidBody : public PhysicsComponent
+class CircleCollider : public Collider
 {
 public:
+	RTTI("CircleCollider");
+
+	//!
+	void SetRadius(float _radius);
+
+	//!
+	void _DebugDraw(void) override;
 
 protected:
+	//!
+	b2Shape* _Shape(void) override { return &m_shape; }
+	//!
+	void _UpdateShape(void) override;
+
+	float m_radius = 1;
+	b2CircleShape m_shape;
+};
+
+//----------------------------------------------------------------------------//
+// BoxCollider
+//----------------------------------------------------------------------------//
+
+class BoxCollider : public Collider
+{
+public:
+	RTTI("BoxCollider");
+
+	//!
+	void SetRect(const Rect& _rect);
+
+	//!
+	void _DebugDraw(void) override;
+
+protected:
+	//!
+	b2Shape* _Shape(void) override { return &m_shape; }
+	//!
+	void _UpdateShape(void) override;
+
+	Rect m_rect = Rect::Identity;
+	b2PolygonShape m_shape;
+};
+
+//----------------------------------------------------------------------------//
+// EdgeCollider
+//----------------------------------------------------------------------------//
+
+class EdgeCollider : public Collider
+{
+public:
+	RTTI("EdgeCollider");
+
+	//!
+	void Set(const Vector2& _v0, const Vector2& _v1);
+	//!
+	uint NumPoints(void) { return 2; }
+	//!
+	Vector2* Points(void) { return m_points; }
+	//!
+	Vector2& Point(uint _index) { ASSERT(_index < 2); return m_points[_index]; }
+	//! 
+	void ApplyPoints(void);
+
+	//!
+	void _DebugDraw(void) override;
+
+protected:
+	//!
+	b2Shape* _Shape(void) override { return &m_shape; }
+	//!
+	void _UpdateShape(void) override;
+
+	Vector2 m_points[2];
+	b2EdgeShape m_shape;
+};
+
+//----------------------------------------------------------------------------//
+// PolygonCollider
+//----------------------------------------------------------------------------//
+
+class PolygonCollider : public Collider
+{
+public:
+	RTTI("PolygonCollider");
+
+	//!
+	void _DebugDraw(void) override;
+
+protected:
+	//!
+	b2Shape* _Shape(void) override { return &m_shape; }
+	//!
+	void _UpdateShape(void) override;
+
+	b2PolygonShape m_shape;
+};
+
+//----------------------------------------------------------------------------//
+// ChainCollider
+//----------------------------------------------------------------------------//
+
+class ChainCollider : public Collider
+{
+public:
+	RTTI("ChainCollider");
+
+	//!
+	void _DebugDraw(void) override;
+
+protected:
+	//!
+	b2Shape* _Shape(void) override { return &m_shape; }
+	//!
+	void _UpdateShape(void) override;
+
+	b2ChainShape m_shape;
+};
+
+//----------------------------------------------------------------------------//
+// Body
+//----------------------------------------------------------------------------//
+
+class Body : public PhysicsComponent
+{
+public:
+	RTTI("Body");
+
+	enum class Type
+	{
+		Static, 
+		Kinematic,
+		Dynamic,
+	};
+
+	//!
+	b2Body* _Handle(void) { return m_body; }
+
+protected:
+	friend class PhysicsWorld;
+
+	//!
+	void _Bind(void) override;
+	//!
+	void _Unbind(void) override;
+	//!
+	void _Enable(void) override;
+	//!
+	void _Disable(void) override;
+	
+	//!
+	void _Create(void);
+	//!
+	void _Destroy(void);
+
+	Body* m_prevBody = nullptr;
+	Body* m_nextBody = nullptr;
+
+	b2Body* m_body = nullptr;
 };
 
 //----------------------------------------------------------------------------//
 // PhysicsWorld
 //----------------------------------------------------------------------------//
 
-class PhysicsWorld : public SceneSystem
+class PhysicsWorld : public SceneSystem, public b2ContactListener
 {
 public:
 	RTTI("PhysicsWorld");
 
+	//!
 	static void Register(void);
 
+	//!
+	PhysicsWorld(void);
+	//!
+	~PhysicsWorld(void);
+
+	//!
+	b2World* _Handle(void) { return &m_world; }
+
 protected:
+	//!
+	void _Start(Scene* _scene) override;
+	//!
+	void _Stop(void) override;
+	//!
+	void _AddComponent(Component* _component) override;
+	//!
+	void _RemoveComponent(Component* _component) override;
 	//!
 	void _Update(void) override;
 	//!
 	void _PostUpdate(void) override;
+
+	//!
+	void BeginContact(b2Contact* _contact) override;
+	//!
+	void EndContact(b2Contact* _contact) override;
+	//!
+	void PreSolve(b2Contact* _contact, const b2Manifold* _oldManifold) override;
+	//!
+	void PostSolve(b2Contact* _contact, const b2ContactImpulse* _impulse) override;
+
+
+	Body* m_firstBody = nullptr;
+	b2World m_world;
 };
 
 //----------------------------------------------------------------------------//
